9363e660eaffa848c07041fe62275056f195fdde
Juan Díaz <juan.diaz.12069@gmail.com>
2019-07-17 18:11:28 -0400

    video 32 completed, added a helper file

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,11 +3,25 @@
 @section('content')
 
     <header class="flex items-center mb-3 py-4">
-        <div class="flex justify-between w-full items-end">
+        <div class="flex justify-between items-end w-full">
             <p class="text-gray-500 text-sm font-normal">
                <a href="/projects" class="text-gray-500 text-sm font-normal no-underline">My Proyects</a> / {{$project->title}}
             </p>
-            <a href="{{ $project->path() . '/edit' }}" class="button">Edit Project</a>
+            <div class="flex items-center">
+                @foreach ($project->members as $member)
+                    <img
+                        src="{{gravatar_url($member->email)}}"
+                        alt="{{ $member->name }}'s avatar'"
+                        class="rounded-full w-8 mr-2">
+                @endforeach
+
+                <img
+                    src="{{gravatar_url($project->owner->email)}}"
+                    alt="{{ $project->owner->name }}'s avatar'"
+                    class="rounded-full w-8 mr-2">
+
+                <a href="{{ $project->path() . '/edit' }}" class="button ml-4">Edit Project</a>
+            </div>
         </div>
     </header>
 
