56dbdbb96c2e8ca13ed246482f4aa57ef9861ab6
Juan Díaz <juan.diaz.12069@gmail.com>
2019-07-18 11:32:33 -0400

    Video 33 completed, added formrequest

--- tests/Feature/InvitationsTest.php
+++ tests/Feature/InvitationsTest.php
@@ -13,8 +13,48 @@ class InvitationsTest extends TestCase
 {
     use RefreshDatabase;
 
+
     /** @test */
-    public function a_project_can_invite_a_user()
+    public function non_owners_may_not_invite_users()
+    {
+        #$this->withoutExceptionHandling();
+
+        $project = ProjectFactory::create();
+        $user = factory(User::class)->create();
+        $this->actingAs($user)
+            ->post($project->path() . '/invitations')
+            ->assertStatus(403);
+    }
+
+
+    /** @test */
+    public function a_project_owner_can_invite_a_user()
+    {
+        $this->withoutExceptionHandling();
+        $project = ProjectFactory::create();
+        $userToInvite = factory(User::class)->create();
+
+        $this->actingAs($project->owner)->post($project->path() . '/invitations', [
+            'email' => $userToInvite->email
+        ])->assertRedirect($project->path());
+        $this->assertTrue($project->members->contains($userToInvite));
+    }
+
+    /** @test */
+    public function the_email_address_must_be_associated_with_a_valid_account()
+    {
+        $project = ProjectFactory::create();
+
+        $this->actingAs($project->owner)->post($project->path() . '/invitations', [
+            'email' => 'notuser@example.cl'
+        ])->assertSessionHasErrors([
+            'email' => 'The user you are inviting must have a Birdboard account'
+        ]);
+    }
+
+
+    /** @test */
+    public function invited_users_may_update_project_details()
     {
         $project = ProjectFactory::create();
         $project->invite($newUser = factory(User::class)->create());
@@ -23,7 +63,6 @@ class InvitationsTest extends TestCase
         $this->post(action('ProjectTasksController@store', $project), $task = ['body' => 'add a new task']);
 
         $this->assertDatabaseHas('tasks', $task);
-
     }
 
 }
